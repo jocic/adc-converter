234ced7c0f2567428f0637e7d36a2cf9b98a1594
Đorđe Jocić <office@djordjejocic.com>
2023-07-29 22:47:20 +0200

    SR Model

--- source/options/sample-rate/sr_view.cpp
+++ source/options/sample-rate/sr_view.cpp
@@ -5,22 +5,23 @@
 #include <QLineEdit>>
 
 #include "mvc/element_manager.h"
+#include "sr_model.h"
 #include "sr_view.h"
 
 void SrView::initialize(QWidget* parent) {
     
     // Arrange View
     
-    QGroupBox*   box     = new QGroupBox();
-    QFormLayout* layout  = new QFormLayout();
-    QLineEdit*   samples = new QLineEdit();
+    QGroupBox*   box    = new QGroupBox();
+    QFormLayout* layout = new QFormLayout();
+    QLineEdit*   rate   = new QLineEdit();
     
     box->setLayout(layout);
     box->setTitle("Sample Rate");
     
-    samples->setText("44100");
+    rate->setText("44100");
     
-    layout->addRow(samples);
+    layout->addRow(rate);
     
     parent->layout()->addWidget(box);
     
@@ -28,8 +29,7 @@ void SrView::initialize(QWidget* parent) {
     
     ElementManager* manager = this->get_ElementManager();
     
-    //manager->push(BpsModel::FIELD_BITS_PER_SAMPLE, combo);
-    //manager->push(BpsModel::FIELD_SIGNED, check);
+    manager->push(SrModel::FIELD_SAMPLE_RATE, rate);
     
     emit SrView::sig_View_Initialized(manager);
 }
