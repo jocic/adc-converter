6d1b400241b1d7964b133e54f99d79d961213e1b
Đorđe Jocić <office@djordjejocic.com>
2023-07-31 08:12:07 +0200

    PL Model

---
+++ source/options/playback/pl_model.h
@@ -0,0 +1,17 @@
+#ifndef ADC_OPTIONS_PL_MODEL
+
+#define ADC_OPTIONS_PL_MODEL
+
+#include "mvc/abstract_model.h"
+
+class PlModel : public AbstractModel {
+    
+    public:
+        
+        static const QString FIELD_PLAYBACK_TIME;
+        
+        qreal get_PlaybackTime();
+        void set_PlaybackTime(qreal value);
+};
+
+#endif
