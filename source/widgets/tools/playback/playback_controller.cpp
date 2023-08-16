8ebabe41d1ab101d95e6533ae8e9d096f5b94ec0
Đorđe Jocić <office@djordjejocic.com>
2023-08-16 06:16:40 +0200

    Removed old audio export implementation.

--- source/widgets/tools/playback/playback_controller.cpp
+++ source/widgets/tools/playback/playback_controller.cpp
@@ -5,8 +5,6 @@
 #include <QMessageBox>
 #include <QPushButton>
 
-#include <format.h>
-
 #include "playback_model.h"
 #include "playback_view.h"
 #include "playback_controller.h"
@@ -83,72 +81,5 @@ void PlaybackController::on_Clicked_Toggle() {
 }
 
 void PlaybackController::on_Clicked_Export() {
-    
-    WAV_FILE    export_wav;
-    QFileDialog export_dialog;
-    QString     export_filename;
-    quint8      export_bytes;
-    
-    export_filename = "export_" + QDateTime::currentDateTime().toString("yyyyMMdd_hhmmss") + ".wav";
-    
-    export_dialog.setWindowTitle("Export Audio");
-    export_dialog.setAcceptMode(QFileDialog::AcceptSave);
-    export_dialog.selectFile(export_filename);
-    export_dialog.exec();
-    
-    if (export_dialog.result() == 0) { // Cancelled
-        return;
-    }
-    
-    if (export_dialog.selectedFiles().size() == 0) {
-        QMessageBox* alert = new QMessageBox();
-        alert->setWindowTitle("I/O Warning");
-        alert->setText("You haven't selected a file.");
-        alert->exec();
-        return;
-    }
-    
-    QString export_file_path = export_dialog.selectedFiles().front();
-    
-    export_wav   = wav_open(export_file_path.toStdString().c_str(), WAV_NEW);
-    export_bytes = m_BitsPerSample / 8;
-    
-    if (!wav_is_open(&export_wav)) {
-        QMessageBox* alert = new QMessageBox();
-        alert->setWindowTitle("I/O Error");
-        alert->setText("File couldn't be generated.");
-        alert->exec();
-        return;
-    }
-    
-    wav_set_defaults(&export_wav, 1);
-    wav_set_SampleRate(&export_wav, m_SampleRate);
-    wav_set_BitsPerSample(&export_wav, m_BitsPerSample);
-    
-    QByteArray* buffer = AppLoader::get_Instance()->get_Buffer();
-    
-    WAV_PCM32 sample = 0;
-    
-    quint8 ctl = export_bytes;
-    
-    for (const auto& byte : *buffer) {
-        
-        sample <<= 8;
-        sample  |= byte;
-        
-        if (--ctl == 0) {
-            
-            wav_push_sample(&export_wav, &sample, NULL);
-            
-            sample = 0;
-            ctl    = export_bytes;
-        }
-    }
-    
-    wav_close(&export_wav);
-    
-    QMessageBox* alert = new QMessageBox();
-    alert->setWindowTitle("App Notice");
-    alert->setText("Audio exported.");
-    alert->exec();
+    qDebug() << "export";
 }
